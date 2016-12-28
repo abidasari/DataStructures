55fd89db578f6e6b7b41fb0a9aba637d6ebfdbd6
adasari <adasari@usc.edu>
2016-12-28 14:54:45 +0530

    Find the modular node from the end.

--- src/linkedlist/ModularNode.java
+++ src/linkedlist/ModularNode.java
@@ -11,7 +11,7 @@ public class ModularNode<T> {
         if(head == null)
             return null;
         SLLNode<T> temp = head, out = null;
-        int index = 0;
+        int index = 1;
         while(temp != null){
             if((index % k) == 0)
                 out = temp;
@@ -21,12 +21,26 @@ public class ModularNode<T> {
         return out;
     }
 
+    public SLLNode<T> findModularNodeFromEnd(SLLNode<T> head, int k) {
+        if(head == null || k < 0)
+            return null;
+        SLLNode<T> temp = head, out = head;
+        for(int i = 0; i < k; i++)
+            temp = temp.getNext();
+
+        while(temp.getNext() != null){
+            out = out.getNext();
+            temp = temp.getNext();
+        }
+        return out;
+    }
+
     public static void main(String[] args) {
         SingleLinkList<Integer> list = new SingleLinkList<Integer>();
         ModularNode<Integer> obj = new ModularNode<>();
-        for(int i = 19; i >= 0; i--)
+        for(int i = 19; i >= 1; i--)
             list.insertAtBegin(i);
         System.out.println(list);
-        System.out.println("Modular Node is [ " + obj.findModularNode(list.getHead(), 5).getData() + " ]");
+        System.out.println("Modular Node is [ " + obj.findModularNodeFromEnd(list.getHead(), 3).getData() + " ]");
     }
 }
