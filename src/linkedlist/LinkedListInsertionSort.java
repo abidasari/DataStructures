d6ca8b0109c3cc04b0569ecd8879c73bc5440788
adasari <adasari@usc.edu>
2017-01-05 15:31:06 +0530

    In place insertion sort.

--- src/linkedlist/LinkedListInsertionSort.java
+++ src/linkedlist/LinkedListInsertionSort.java
@@ -48,24 +48,54 @@ public class LinkedListInsertionSort<T extends Number> {
         return list;
     }
 
+    public SLLNode<T> insertionSortInPlace(SLLNode<T> head){
+        if(head == null || head.getNext() == null)
+            return head;
+        SLLNode<T> sortedHead = head, sortedTail = head, toBeSorted = head.getNext();
+        while(toBeSorted != null){
+            SLLNode<T> temp = head, prev = null;
+            while(temp != toBeSorted && temp.getData().intValue() < toBeSorted.getData().intValue()){
+                prev = temp;
+                temp = temp.getNext();
+            }
+            if(temp == head){ // first location
+                sortedTail.setNext(toBeSorted.getNext());
+                toBeSorted.setNext(sortedHead);
+                sortedHead = toBeSorted;
+                toBeSorted = sortedTail.getNext();
+                head = sortedHead;
+            } else if(temp == toBeSorted){ // last location
+                sortedTail = toBeSorted;
+                toBeSorted = toBeSorted.getNext();
+            } else { // somewhere in the middle
+                SLLNode<T> toBeSortedNext = toBeSorted.getNext();
+                sortedTail.setNext(toBeSortedNext);
+                prev.setNext(toBeSorted);
+                toBeSorted.setNext(temp);
+                toBeSorted = toBeSortedNext;
+            }
+        }
+        return head;
+    }
+
     public static void main(String[] args) {
         SingleLinkList<Integer> list = new SingleLinkList<Integer>();
         LinkedListInsertionSort<Integer> obj = new LinkedListInsertionSort<>();
 
-        list.insertAtBegin(11);
-        list.insertAtBegin(12);
-        list.insertAtBegin(13);
-        list.insertAtBegin(12);
-        list.insertAtBegin(56);
+//        list.insertAtBegin(11);
+//        list.insertAtBegin(12);
+//        list.insertAtBegin(13);
+//        list.insertAtBegin(12);
+//        list.insertAtBegin(56);
         list.insertAtBegin(21);
         list.insertAtBegin(9);
-        list.insertAtBegin(23);
-        list.insertAtEnd(14);
+//        list.insertAtBegin(23);
+//        list.insertAtEnd(14);
 
 
         System.out.println("List : " + list + " Size: " + list.size());
 
-        System.out.println("Sorted list : " + obj.printList(obj.insertionSort(list.getHead())));
+        System.out.println("Sorted list : " + obj.printList(obj.insertionSortInPlace(list.getHead())));
 
 
     }
