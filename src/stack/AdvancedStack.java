6d5bb16f468cd6fd87f48cab86d9c6c235856c8e
adasari <adasari@usc.edu>
2017-01-07 22:57:55 +0530

    Better implementation. Uses less Space

--- src/stack/AdvancedStack.java
+++ src/stack/AdvancedStack.java
@@ -19,6 +19,21 @@ public class AdvancedStack{
             minStack.push(input);
     }
 
+    public void betterPush(int input){
+        mainStack.push(input);
+        if(!minStack.isEmpty() && minStack.peek() < input){
+            assert true;
+        }
+        else
+            minStack.push(input);
+    }
+
+    public int betterPop(){
+        if(mainStack.peek() == minStack.peek())
+            minStack.pop();
+        return mainStack.pop();
+    }
+
     public int pop(){
         minStack.pop();
         return mainStack.pop();
@@ -38,16 +53,16 @@ public class AdvancedStack{
 
     public static void main(String[] args) {
         AdvancedStack obj = new AdvancedStack();
-        obj.push(4);
-        obj.push(5);
-        obj.push(9);
-        obj.push(90);
-        obj.push(21);
-        obj.push(3);
-        obj.push(23);
+        obj.betterPush(4);
+        obj.betterPush(5);
+        obj.betterPush(9);
+        obj.betterPush(90);
+        obj.betterPush(21);
+        obj.betterPush(3);
+        obj.betterPush(23);
 
-        obj.pop();
-        obj.pop();
+        obj.betterPop();
+        obj.betterPop();
         System.out.println(obj.getMinimum());
 
     }
