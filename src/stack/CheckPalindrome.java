ba3f06bc4b728e424912cca568cea861b67f9a08
adasari <adasari@usc.edu>
2017-01-08 01:32:02 +0530

    Check if a given string is a palindrome

---
+++ src/stack/CheckPalindrome.java
@@ -0,0 +1,49 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * @author adasari
+ */
+
+public class CheckPalindrome {
+
+    //given a string marked with an "X" as the mid-point check if it is a palindrome
+
+    public boolean isPalindromeA(String input){ // uses iterative approach
+        int i = 0, j = input.length() - 1;
+        while(i < j && input.charAt(i) == input.charAt(j)){
+            i++;
+            j--;
+        }
+        if(i < j)
+            return false;
+        else
+            return true;
+    }
+
+    public boolean isPalindromeB(String input){ // Stack Approach
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while(input.charAt(i) != 'X'){
+            stack.push(input.charAt(i));
+            i++;
+        }
+        i++;
+        while(!stack.isEmpty()){
+            if(input.charAt(i) != stack.pop())
+                return false;
+            i++;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+//        String test = "aabbaaabaXabaaabbaa";
+        String test = "aXb";
+        CheckPalindrome obj = new CheckPalindrome();
+
+        System.out.println(test + " is a palindrome: " + obj.isPalindromeB(test) );
+
+    }
+}
