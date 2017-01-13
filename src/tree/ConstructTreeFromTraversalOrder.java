5bae3ef87c1a02bc91c09ac3ef268bb3007a03d6
adasari <adasari@usc.edu>
2017-01-13 18:15:39 +0530

    Add LevelOrder traversal

--- src/tree/ConstructTreeFromTraversalOrder.java
+++ src/tree/ConstructTreeFromTraversalOrder.java
@@ -6,6 +6,8 @@ package tree;
 
 import nodes.BTNode;
 
+import java.util.Arrays;
+
 public class ConstructTreeFromTraversalOrder<T extends Number>{
 
     public BTNode<T> constructFromInPre(T[] inOrder, T[] preOrder){
@@ -56,13 +58,50 @@ public class ConstructTreeFromTraversalOrder<T extends Number>{
         return node;
     }
 
+//    public BTNode<T> constructFromInLevel(T[] inOrder, T[] levelOrder){
+//        if(inOrder.length == 0 || inOrder.length != levelOrder.length)
+//            return null;
+//        return buildTreeLevel(levelOrder, inOrder, 0, inOrder.length - 1);
+//    }
+//
+//    public BTNode<T> buildTreeLevel(T[] levelOrder, T[] inOrder, int inStart, int inEnd){
+//        if( levelOrder.length == 0 || inStart > inEnd)
+//            return null;
+//        T data = levelOrder[0];
+//        BTNode<T> node = new BTNode<T>(data);
+//        int offSet = inStart;
+//        for(; offSet < inEnd; offSet++)
+//            if(inOrder[offSet] == data)
+//                break;
+//        node.setLeft(
+//                buildTreeLevel(subSet(levelOrder, inOrder, inStart, offSet - 1), inOrder, inStart, offSet - 1)
+//        );
+//        node.setRight(
+//                buildTreeLevel(subSet(levelOrder, inOrder, offSet + 1, inEnd), inOrder, offSet + 1, inEnd)
+//        );
+//        return node;
+//    }
+//
+//    public T[] subSet(T[] level, T[] in, int inStart, int inEnd){
+//        T[] out = (T[])new Object[inEnd - inStart + 1]; //Arrays.asList(yourArray).contains(yourValue)
+//        int index = 0;
+//        for(int i = 0; i < level.length; i++){
+//            if(Arrays.asList(in).contains(level[i])){
+//                out[index] = level[i];
+//                index++;
+//            }
+//        }
+//        return out;
+//    }
+
+
     public static void main(String[] args) {
         ConstructTreeFromTraversalOrder<Integer> obj = new ConstructTreeFromTraversalOrder<>();
         BinaryTree<Integer> tree = new BinaryTree<>();
 
-        Integer[] post = {4,2,5,6,3,1};
+        Integer[] level = {1,2,3,4,5,6};
         Integer[] in = {4,2,1,5,3,6};
-        tree.setRoot(obj.constructFromInPost(in, post));
+        tree.setRoot(obj.constructFromInPost(in, level));
         tree.print2D();
     }
 }
