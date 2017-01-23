d854cb316e9d614f3ff685c12262135793726a02
adasari <adasari@usc.edu>
2017-01-22 18:10:31 -0800

    Trim a tree to remove out of range members

--- src/tree/bst/BSTNumbersInRange.java
+++ src/tree/bst/BSTNumbersInRange.java
@@ -22,9 +22,22 @@ public class BSTNumbersInRange{
             numbersInRange(node.getLeft(), j, k);
     }
 
+    public BSTNode trimTreeInRange(BSTNode node, int min, int max){
+        if(node == null)
+            return null;
+        node.setLeft(trimTreeInRange(node.getLeft(), min, max));
+        node.setRight(trimTreeInRange(node.getRight(), min, max));
+        if(node.getData() <= min)
+            return node.getRight();
+        if(node.getData() >= max)
+            return node.getLeft();
+        return node;
+    }
+
     public static void main(String[] args){
         BSTNumbersInRange obj = new BSTNumbersInRange();
         BinarySearchTree tree = new BinarySearchTree();
+        BinarySearchTree tree2 = new BinarySearchTree();
         tree.insert(6);
         tree.insert(4);
         tree.insert(2);
@@ -34,7 +47,9 @@ public class BSTNumbersInRange{
         tree.insert(1);
         tree.print2D();
         System.out.println("========================================");
-        obj.numbersInRange(tree.getRoot(), 4, 10);
+
+        tree2.setRoot(obj.trimTreeInRange(tree.getRoot(), 2, 10));
+        tree2.print2D();
 
     }
 }
